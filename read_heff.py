fc3142d475b21e8675c7e1b65ebf22c63e67bf17
LeraS <lera.selyzenok@gmail.com>
2017-08-02 22:30:53 +0300

    reading one month. sizes are hardcoded

--- read_heff.py
+++ read_heff.py
@@ -7,23 +7,23 @@ FILENAME = 'heff.H2016'
 
 #bformat= ">%sh"
 
-bformat= "=f"
+bformat= "=43200f"
 
 ns = 360*120*4
 f = open(FILENAME, 'rb')
 byte_arr=f.read(ns)
 f.close()
-unpacked_bytes = unpack(bformat % (ns/4), byte_arr)
-data=np.array(unpacked_bytes).reshape(360,120)
+unpacked_bytes = unpack(bformat, byte_arr)
+data=np.array(unpacked_bytes).reshape(120,360)
 
 
-from scipy.io import FortranFile
-
-ff = FortranFile(FILENAME , 'r')
-a = ff.read_reals(dtype='f8')
-
-
-import numpy as np
-with open(FILENAME,'rb') as nf:
-    for k in xrange(120):
-        data = np.fromfile(nf, dtype=np.float32, count = 120)
+#from scipy.io import FortranFile
+#
+#ff = FortranFile(FILENAME , 'r')
+#a = ff.read_reals(dtype='f8')
+#
+#
+#import numpy as np
+#with open(FILENAME,'rb') as nf:
+#    for k in xrange(120):
+#        data = np.fromfile(nf, dtype=np.float32, count = 120)
